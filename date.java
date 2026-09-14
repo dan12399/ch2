018bfddc4259a0af3b8aea6e9d65c6ef1bc4afb2
dan12399 <2555089@learn.vsb.bc.ca>
2026-09-14 13:29:12 -0700

    Add files via upload

---
+++ date.java
@@ -0,0 +1,23 @@
+public class date{
+	
+	public static void main (String[] args) {
+		
+		//variable declarations
+		String day;
+		day = "Monday";
+		int date = 14;
+		String month = "September";
+		int year = 2026;
+		
+		//printing variables
+		System.out.print("The current date is");
+		System.out.print(day);
+		System.out.print(,);
+		System.out.print(date);
+		System.out.print(,);
+		System.out.print(month);
+		System.out.print(,);
+		System.out.print(year);
+		System.out.print(.);
+	}
+}
