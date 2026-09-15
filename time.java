ae659f3d55679542eb9748b009b93dc29d3d9a26
dan12399 <2555089@learn.vsb.bc.ca>
2026-09-15 13:12:04 -0700

    Add files via upload

---
+++ time.java
@@ -0,0 +1,20 @@
+public class time{
+
+	public static void main (String[] args) {
+		
+		//variable declarations
+		Int hour;
+		hour = "13":
+		Int minute;
+		minute = "02";
+		Int second;
+		second = "48";
+		
+		//printing variables
+		System.out.print("The current time is");
+		System.out.print(": ");
+		System.out.println("hour"+":"+"minute"+":"+"second")
+		
+		System.out.print(Number of seconds since midnight);
+	}
+}
